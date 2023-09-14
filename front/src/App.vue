60f8103d2b0e5ec653b623d797111fcb0bb014f2
Marc Gotze marc.gotze@gmail.com <marc.gotze@gmail.com>
2023-09-14 11:25:26 +0200

    routes & users view init

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,9 @@
 <script setup>
- import Banner from './components/Banner.vue';
- import NavBar from './components/NavBar.vue';
- import Movies from './pages/Movies.vue'
+  import Movies from './pages/Movies.vue'
 </script>
 
 <template>
   <div class="wrapper">
-    <Banner />
-    <NavBar />
     <Movies />
   </div>
 </template>
