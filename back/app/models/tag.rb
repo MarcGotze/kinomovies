0dc2c18084bb0173779c399dd09910178739b458
Marc Gotze marc.gotze@gmail.com <marc.gotze@gmail.com>
2023-09-15 12:43:28 +0200

    dependent destroy added to models

--- back/app/models/tag.rb
+++ back/app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
-  has_many :liked_tags
-  has_many :disliked_tags
+  has_many :liked_tags, dependent: :destroy
+  has_many :disliked_tags, dependent: :destroy
   has_many :users, through: :liked_tags
   has_many :users, through: :disliked_tags
 end
--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :liked_tags
-  has_many :disliked_tags
+  has_many :liked_tags, dependent: :destroy
+  has_many :disliked_tags, dependent: :destroy
   has_many :tags, through: :liked_tags
   has_many :tags, through: :disliked_tags
 end
